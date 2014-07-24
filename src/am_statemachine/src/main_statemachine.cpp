d003ed9b8fae0a11bb273d6655be969456aaad15
Robert Wittmann <robert.wittmann@tum.de>
2014-07-24 15:52:26 +0000

    test actionlib

--- src/am_statemachine/src/main_statemachine.cpp
+++ src/am_statemachine/src/main_statemachine.cpp
@@ -1,14 +1,17 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
 #include <sstream>
+
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/simple_client_goal_state.h>
+#include <actionlib/client/terminal_state.h>
+#include <am_statemachine/testAction.h>
 
 
 int main(int argc, char **argv)
 {
   //init ros system, register as node "sm"
-  ros::init(argc, argv, "sm");
+  ros::init(argc, argv, "statemachine");
 
   /*
    * NodeHandle is the main access point to communications with the ROS system.
@@ -16,6 +19,8 @@ int main(int argc, char **argv)
    * NodeHandle destructed will close down the node.
    */
   ros::NodeHandle sm_node;
+
+  actionlib::SimpleActionClient<am_statemachine::testAction> ac("test_node", true);
 
   //inform Master that program wants to publish on the topic "sm_pub"
   //->returns a publisher object
@@ -28,14 +33,43 @@ int main(int argc, char **argv)
 
   int64_t count=0;
 
+  ROS_INFO("Waiting for action server to start.");
+  // wait for the action server to start
+  ac.waitForServer(); //for infinite time
+  ROS_INFO("Action server started, sending goal.");
+
+  am_statemachine::testGoal goal;
+  goal.blub = 10;
+  ac.sendGoal(goal);
+
+  bool finished=false;
   //main loop
-  while(ros::ok())
+  while(ros::ok() && finished)
   {
 	  std_msgs::String msg;
 
 	  msg.data = "Hello World";
 
-	  ROS_INFO("%s",msg.data.c_str());
+	  if(count%100==0)
+		  ROS_INFO("%s",msg.data.c_str());
+
+	  actionlib::SimpleClientGoalState state = ac.getState();
+	  //PENDING, ACTIVE, RECALLED, REJECTED, PREEMPTED, ABORTED, SUCCEEDED, LOST
+	  if(state != actionlib::SimpleClientGoalState::ACTIVE)
+	  {
+		  switch(state.state_)
+		  {
+		  case actionlib::SimpleClientGoalState::SUCCEEDED:
+			  finished=true;
+			  break;
+		  case actionlib::SimpleClientGoalState::ABORTED:
+			  ROS_INFO("action aborted!");
+			  break;
+		  default:
+			  ROS_INFO("something wrong happened");
+			  break;
+		  }
+	  }
 
 	  //send a message
 	  sm_pub.publish(msg);
@@ -45,6 +79,9 @@ int main(int argc, char **argv)
 
 	  //sleep for the remaining time
 	  loop_rate.sleep();
-	  ++count;
+	  count++;
   }
+  ROS_INFO("ACTION succeeded!!!");
+
+  return 0;
 }
---
+++ src/am_statemachine/src/main_testnode.cpp
@@ -0,0 +1,88 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <am_statemachine/testAction.h>
+
+
+class TestNode
+{
+	ros::NodeHandle nh_;
+    // NodeHandle instance must be created before this line. Otherwise strange error may occur.
+    actionlib::SimpleActionServer<am_statemachine::testAction> as_;
+    std::string action_name_;
+
+    // create messages that are used to published feedback/result
+    am_statemachine::testFeedback feedback_;
+    am_statemachine::testResult result_;
+
+public:
+
+  TestNode(std::string name) :
+    as_(nh_, name, boost::bind(&TestNode::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    as_.start();
+  }
+
+  ~TestNode(void)
+  {
+  }
+
+  void executeCB(const am_statemachine::testGoalConstPtr &goal)
+    {
+	  ros::Rate r(1);
+	  bool success = true;
+
+	  // push_back the seeds for the fibonacci sequence
+	  feedback_.sequence.clear();
+	  feedback_.sequence.push_back(0);
+	  feedback_.sequence.push_back(1);
+
+	  ROS_INFO("STARTING executeCB ...");
+
+	  uint32_t counter=0;
+	  bool active=true;
+	  while(active)
+	  {
+		  if (as_.isPreemptRequested() || !ros::ok())
+		  {
+			  ROS_INFO("%s: Preempted", action_name_.c_str());
+			  // set the action state to preempted
+			  as_.setPreempted();
+			  success = false;
+			  break;
+		  }
+
+		  counter++;
+		  feedback_.sequence.push_back(counter);
+		  as_.publishFeedback(feedback_);
+
+		  ROS_INFO("FEEDBACK: %i",counter);
+
+		  r.sleep();
+
+		  if(counter==goal->blub)
+			  active=false;
+	  }
+
+	  if(success)
+	  	{
+	  	  result_.sequence = feedback_.sequence;
+	  	  ROS_INFO("%s: Succeeded", action_name_.c_str());
+	  	  // set the action state to succeeded
+	  	  as_.setSucceeded(result_);
+	  	}
+    }
+};
+
+int main(int argc, char **argv)
+{
+	ros::init(argc,argv,"test_node");
+
+	ros::NodeHandle node;
+
+	//subscribe to message
+	TestNode test_node(ros::this_node::getName());
+	ros::spin();
+
+	return 0;
+}
