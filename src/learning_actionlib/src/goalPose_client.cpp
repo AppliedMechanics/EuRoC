1e99ae18aeb2647e1b9c9259b31250df9afa1264
Christoph Schütz <christoph.schuetz@tum.de>
2014-07-30 18:20:56 +0000

    dummy client for action created

---
+++ src/learning_actionlib/src/goalPose_client.cpp
@@ -0,0 +1,44 @@
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <am_msgs/goalPoseAction.h>
+
+int main (int argc, char **argv)
+{
+  ros::init(argc, argv, "test_motionplanning");
+
+  // create the action client
+  // true causes the client to spin its own thread
+  actionlib::SimpleActionClient<am_msgs::goalPoseAction> ac("goalPoseAction", true);
+
+  ROS_INFO("Waiting for action server to start.");
+  // wait for the action server to start
+  ac.waitForServer(); //will wait for infinite time
+
+  ROS_INFO("Action server started, sending goal.");
+  // send a goal to the action
+  am_msgs::goalPoseGoal goal;
+  geometry_msgs::Pose example_pose;
+  example_pose.orientation.w = 1.0;
+  example_pose.position.x = 0.5;
+  example_pose.position.y = 0.5;
+  example_pose.position.z = 0.3;
+
+  goal.goal_pose = example_pose;
+  ac.sendGoal(goal);
+
+  //wait for the action to return
+  bool finished_before_timeout = ac.waitForResult(ros::Duration(300.0));
+
+  if (finished_before_timeout)
+  {
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("Action finished: %s",state.toString().c_str());
+  }
+  else
+    ROS_INFO("Action did not finish before the time out.");
+
+  //exit
+  return 0;
+}
+
