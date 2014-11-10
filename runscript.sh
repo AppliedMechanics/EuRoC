ad9c9883e4083f0e0c8a0650798940b777091e60
Robert Wittmann <robert.wittmann@tum.de>
2014-11-10 17:07:32 +0000

    updated runscript

--- runscript.sh
+++ runscript.sh
@@ -1,104 +1,30 @@
 #source /opt/ros/hydro/setup_client.bash
 #source /opt/euroc_c2s1/code/devel/setup.bash
 
-name="task1_v1"
-echo "starting " $name
-export RELAUNCH=TRUE
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-if [ "$RELAUNCH" == "TRUE" ]
-  then
-    echo "relaunching " $name
-    roslaunch am_launchfiles final_eval.launch task_name:=$name
-  else
-    echo "finished " $name
-fi
+array=(task1_v1 task1_v2 task1_v3 task2_v1_1 task2_v1_2 task2_v1_3 task2_v2_1 task2_v2_2 task2_v2_v3 task3_v1 task3_v2 task4_v1_1 task4_v1_2 task4_v1_3 task4_v2_1 task4_v2_2 task4_v2_3)
 
-name="task1_v2"
-echo "starting " $name
-export RELAUNCH=TRUE
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-if [ "$RELAUNCH" == "TRUE" ]
-  then
-    echo "relaunching " $name
+for task in ${array[*]}
+do
+    name=$task
+    echo "starting " $name
+    export RELAUNCH=TRUE
+    export ROS_LOG_DIR=~/roslog/$name
     roslaunch am_launchfiles final_eval.launch task_name:=$name
-  else
-    echo "finished " $name
-fi
+    # if [ "$RELAUNCH" == "TRUE" ]
+    # then
+    # 	echo "relaunching " $name
+    # 	roslaunch am_launchfiles final_eval.launch task_name:=$name
+    # else
+    # 	echo "finished " $name
+    # fi
 
-name="task1_v3"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task2_v1_1"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task2_v1_2"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task2_v1_3"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task3_v1"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task3_v2"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task3_v3"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task4_v1_1"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task4_v1_2"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task4_v1_3"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task4_v2_1"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task4_v2_2"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task4_v2_3"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task4_v3_1"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task4_v3_2"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task2_v3_1"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task2_v3_2"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-# name="task2_v3_3"
-# export ROS_LOG_DIR=~/roslog/$name
-# roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task3_v1"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task3_v2"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task3_v3"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task4_v1_1"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task4_v1_2"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task4_v1_3"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
-name="task5_v1"
-export ROS_LOG_DIR=~/roslog/$name
-roslaunch am_launchfiles final_eval.launch task_name:=$name
+    while [ "$RELAUNCH" == "TRUE" ]
+    do
+	echo "relaunching " $name
+	roslaunch am_launchfiles final_eval.launch task_name:=$name
+    done
+
+    echo "finished " $name
+done
+
+
